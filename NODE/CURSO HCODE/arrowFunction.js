d83e673e793ec154903053f797ff5069501518ec
Filipe Augusto Santos Chaves <67299107+filipe-augusto@users.noreply.github.com>
2020-08-23 20:14:32 -0300

    att

---
+++ NODE/CURSO HCODE/arrowFunction.js
@@ -0,0 +1,8 @@
+let calc = (x1,x2,operador)=>{
+    return eval(`${x1} ${operador} ${x2}`)
+    }
+    let resultado = calc(45,9,"/")
+    console.log(resultado) 
+    
+    
+    
---
+++ NODE/CURSO HCODE/funcaoAnonima.js
@@ -0,0 +1,5 @@
+
+   
+    console.log(( function (x1,x2,operador){
+        return eval(`${x1} ${operador} ${x2}`)
+        })(5,4,"+"))
